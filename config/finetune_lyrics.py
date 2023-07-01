e3caeb802cc208436005d625176eca64190b8618
Ahmad Shallouf <7shallou@informatik.uni-hamburg.de>
2023-07-01 10:45:19 +0200

    generation_output has all the evaluated test data

--- config/finetune_lyrics.py
+++ config/finetune_lyrics.py
@@ -1,17 +1,17 @@
 import time
 import os
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 out_dir = 'out-lyrics'
 eval_interval = 5
-eval_iters = 40
-wandb_log = False # feel free to turn on
+eval_iters = 80
+wandb_log = False  # feel free to turn on
 wandb_project = 'lyrics'
 wandb_run_name = 'ft-' + str(time.time())
 
 dataset = 'lyrics'
-init_from = 'gpt2-medium' # this is the largest GPT-2 model
+init_from = 'gpt2-medium'  # this is the largest GPT-2 model
 
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
@@ -21,7 +21,7 @@ always_save_checkpoint = False
 # shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 1
 gradient_accumulation_steps = 32
-max_iters = 250
+max_iters = 400
 
 # finetune at constant LR
 learning_rate = 3e-5
